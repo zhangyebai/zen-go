d27d811a2af87866884c585d69896b6710cf1a23
zhangyebai <zhangyebai@126.com>
2019-05-20 09:11:56 +0800

    dev-bug fix

--- router/author_router.go
+++ router/author_router.go
@@ -15,10 +15,12 @@ func (authorRouter *AuthorRouter) saveAuthor(ctx *Context) {
 	if err := ctx.ShouldBind(&author); nil != err {
 		log.WithFields(util.BeanToMap(author)).Error("author cant be saved, maybe cause: ", err.Error())
 		ctx.IndentedJSON(http.StatusOK, util.ErrWithMessage(err.Error(), 0))
+		return
 	}
 	if count, err := authorService.SaveAuthor(&author); nil != err {
 		log.WithFields(util.BeanToMap(author)).Error("author cant be saved, maybe cause: ", err.Error())
 		ctx.IndentedJSON(http.StatusOK, util.ErrWithMessage(err.Error(), 0))
+		return
 	} else {
 		ctx.IndentedJSON(http.StatusOK, util.Ok(count))
 	}
